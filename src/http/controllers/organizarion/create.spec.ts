d796011ad4ce56beb449899f68d6011118ac0668
LucieneFagundes <lfnascimento99@gmail.com>
2023-07-10 21:45:42 -0300

    :test_tube: Test e2e - Create Organization

--- src/http/controllers/organizarion/create.spec.ts
+++ src/http/controllers/organizarion/create.spec.ts
@@ -1,3 +1,28 @@
-import { describe } from "vitest";
+import { app } from "src/app";
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-describe("Create an organization e2e", () => {})
+describe("Create an organization e2e", () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+  it("Should be able to create a new organization", async () => {
+    const response = await request(app.server).post('/organizations').send({
+      name: 'Lar animal',
+      email: 'laranimal@gmail.com',
+      password: 'laranimal',
+      telephone: '21988778877',
+      street: 'Rua A',
+      neighborhood: 'Melão',
+      city: 'Cidadela',
+      zip_code: '21232222'
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+});
